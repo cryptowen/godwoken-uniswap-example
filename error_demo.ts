8b69455238769d7bcd5017a678307dc6436ed844
Ethan Hu <cosinlink@163.com>
2021-04-15 03:47:32 +0800

    feat(godwoken-polyjuice v0.1.4): add new rollupTypeHash and new cli call

--- error_demo.ts
+++ error_demo.ts
@@ -3,13 +3,15 @@ import calldataVec from "./error-permission.calldata.json";
 const shell = require('shelljs');
 const PRIVATE_KEY = '0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc';
 const log = console.log.bind(console)
+const rollupTypeHash = '0x40f696276b9013d94076535460723f7b4e3dc66c74b75a123d4c11b8563e7ccb'
 
 
 const main = async () => {
     log('\r\n---begin uniswap demo---');
-    await deposit();  // deposit to account id 2
+    // await deposit();  // deposit to account id 2
+
     // creator account id == 4
-    await createCreatorAccount(PRIVATE_KEY, '0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655', '2');
+    // await createCreatorAccount(PRIVATE_KEY, rollupTypeHash, '2');
 
 
     let index
@@ -17,12 +19,12 @@ const main = async () => {
     // 1. deploy usdt
     index = 0
     log(`\r\n${index}. deploy ${calldataVec[index].name}`)
-    await deploy('4', calldataVec[index].calldata);
+    // await deploy('4', calldataVec[index].calldata);
 
     // 2. deploy demo
     index = 1
     log(`\r\n${index}. deploy ${calldataVec[index].name}`)
-    await deploy('4', calldataVec[index].calldata);
+    // await deploy('4', calldataVec[index].calldata);
 
     // 3. user approve demo, MockUSDT, MaxUint256
     index = 2
@@ -34,16 +36,16 @@ const main = async () => {
     log(`\r\n${index}. ${calldataVec[index].name}`)
     await callSet(`${calldataVec[index].targetAccountId}`, calldataVec[index].calldata)
 
-    // 5. " direct transferFrom, user1 -> demo  998", expect error
-    index = 4
-    log(`\r\n${index}. ${calldataVec[index].name}`)
-    await callSet(`${calldataVec[index].targetAccountId}`, calldataVec[index].calldata)
-
-    // 6. " direct transferFrom, user1 -> demo  998", expect error
-    index = 5
-    log(`\r\n${index}. ${calldataVec[index].name}`)
-    await callSet(`${calldataVec[index].targetAccountId}`, calldataVec[index].calldata)
-
+    // // 5. " direct transferFrom, user1 -> demo  998", expect error
+    // index = 4
+    // log(`\r\n${index}. ${calldataVec[index].name}`)
+    // await callSet(`${calldataVec[index].targetAccountId}`, calldataVec[index].calldata)
+    //
+    // // 6. " direct transferFrom, user1 -> demo  998", expect error
+    // index = 5
+    // log(`\r\n${index}. ${calldataVec[index].name}`)
+    // await callSet(`${calldataVec[index].targetAccountId}`, calldataVec[index].calldata)
+    //
 
     // // 5. balanceOf
     index++
@@ -78,24 +80,24 @@ const sleep = async (seconds: number) => {
     await new Promise((resolve) => setTimeout(resolve, seconds * 1000))
 }
 
-const deploy = async (fromId: string, calldata: string) => {
+const deploy = async (creatorId: string, calldata: string) => {
     const str = `cd /code/godwoken-examples/packages/tools &&  \
-    ts-node-dev ./src/polyjuice-cli.ts deploy ${fromId} ${calldata} \
-    0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc`
+    ts-node-dev ./src/polyjuice-cli.ts deploy ${creatorId} 2000000 0 ${calldata} \
+    ${rollupTypeHash} ${PRIVATE_KEY}`
     const res = shell.exec(str).stdout;
     await sleep(120)
 }
 
 const callGet = async (contractAccountId: string, calldata: string) => {
     const str = `cd /code/godwoken-examples/packages/tools &&  \
-    ts-node-dev ./src/polyjuice-cli.ts staticCall ${contractAccountId} ${calldata} 0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc
+    ts-node-dev ./src/polyjuice-cli.ts staticCall ${contractAccountId} 2000000 0 ${calldata} ${rollupTypeHash} ${PRIVATE_KEY}
     `
     const res = shell.exec(str).stdout;
 }
 
 const callSet = async (contractAccountId: string, calldata: string) => {
     const str = `cd /code/godwoken-examples/packages/tools &&  \
-    ts-node-dev ./src/polyjuice-cli.ts call ${contractAccountId} ${calldata} 0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc
+    ts-node-dev ./src/polyjuice-cli.ts call ${contractAccountId} 2000000 0 ${calldata} ${rollupTypeHash} ${PRIVATE_KEY}
     `
     const res = shell.exec(str).stdout;
     await sleep(100)
--- uniswap_demo.ts
+++ uniswap_demo.ts
@@ -7,12 +7,13 @@ const shell = require('shelljs');
 const LUMOS_CONFIG_FILE='/code/config/lumos-config.json';
 const PRIVATE_KEY = '0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc';
 const log = console.log.bind(console)
+const rollupTypeHash = '0x40f696276b9013d94076535460723f7b4e3dc66c74b75a123d4c11b8563e7ccb'
 
 
 const main = async () => {
     log('\r\n---begin uniswap demo---');
     await deposit();
-    await createCreatorAccount(PRIVATE_KEY, '0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655', '2');
+    await createCreatorAccount(PRIVATE_KEY, rollupTypeHash, '2');
 
     // // 1. WETH  -> account id : 5
     log(`\r\n1. deploy ${calldataVec[0].name}`)
@@ -85,24 +86,48 @@ const sleep = async (seconds: number) => {
     await new Promise((resolve) => setTimeout(resolve, seconds * 1000))
 }
 
-const deploy = async (fromId: string, calldata: string) => {
+// const deploy = async (fromId: string, calldata: string) => {
+//     const str = `cd /code/godwoken-examples/packages/tools &&  \
+//     ts-node-dev ./src/polyjuice-cli.ts deploy ${fromId} ${calldata} \
+//     0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc`
+//     const res = shell.exec(str).stdout;
+//     await sleep(120)
+// }
+//
+// const callGet = async (contractAccountId: string, calldata: string) => {
+//     const str = `cd /code/godwoken-examples/packages/tools &&  \
+//     ts-node-dev ./src/polyjuice-cli.ts staticCall ${contractAccountId} ${calldata} 0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc
+//     `
+//     const res = shell.exec(str).stdout;
+// }
+//
+// const callSet = async (contractAccountId: string, calldata: string) => {
+//     const str = `cd /code/godwoken-examples/packages/tools &&  \
+//     ts-node-dev ./src/polyjuice-cli.ts call ${contractAccountId} ${calldata} 0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc
+//     `
+//     const res = shell.exec(str).stdout;
+//     await sleep(100)
+// }
+
+
+const deploy = async (creatorId: string, calldata: string) => {
     const str = `cd /code/godwoken-examples/packages/tools &&  \
-    ts-node-dev ./src/polyjuice-cli.ts deploy ${fromId} ${calldata} \
-    0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc`
+    ts-node-dev ./src/polyjuice-cli.ts deploy ${creatorId} 2000000 0 ${calldata} \
+    ${rollupTypeHash} ${PRIVATE_KEY}`
     const res = shell.exec(str).stdout;
     await sleep(120)
 }
 
 const callGet = async (contractAccountId: string, calldata: string) => {
     const str = `cd /code/godwoken-examples/packages/tools &&  \
-    ts-node-dev ./src/polyjuice-cli.ts staticCall ${contractAccountId} ${calldata} 0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc
+    ts-node-dev ./src/polyjuice-cli.ts staticCall ${contractAccountId} 2000000 0 ${calldata} ${rollupTypeHash} ${PRIVATE_KEY}
     `
     const res = shell.exec(str).stdout;
 }
 
 const callSet = async (contractAccountId: string, calldata: string) => {
     const str = `cd /code/godwoken-examples/packages/tools &&  \
-    ts-node-dev ./src/polyjuice-cli.ts call ${contractAccountId} ${calldata} 0x9346b517fd137a58d8e266ed7cbb27ba4f693bc792118c2744c3b30b1fcdd655 0xa6b023fec4fc492c23c0e999ab03b01a6ca5524a3560725887a8de4362f9c9cc
+    ts-node-dev ./src/polyjuice-cli.ts call ${contractAccountId} 2000000 0 ${calldata} ${rollupTypeHash} ${PRIVATE_KEY}
     `
     const res = shell.exec(str).stdout;
     await sleep(100)
